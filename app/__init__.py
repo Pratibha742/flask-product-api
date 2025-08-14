9dcbefed4b951f5ca413b44c5ddc928a90a0bcd2
Pratibha Sharma <pratibhasharma0988@gmail.com>
2025-08-14 18:42:50 +0530

    Add root route to home page and postman collection link

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,16 @@ def create_app():
 # Register Blueprints
     app.register_blueprint(api, url_prefix='/api/v1')
 
+    # Root route (home)
+    @app.route("/")
+    def home():
+        return {
+            "message": "Welcome to the Flask Product API",
+            "documentation": "Visit /api/v1 to access the API endpoints",
+            "postman_collection": "https://github.com/Pratibha742/flask-product-api/raw/main/flask_products_api.postman_collection.json"
+        }
+
+
 # Register Error Handlers
     register_error_handlers(app)
     return app
